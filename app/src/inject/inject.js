bf5f921c41909379a4227d2e1683e8d8426efb97
Pietro Marangon <pietro.marangon@gmail.com>
2019-08-22 23:19:07 +0200

    Update inject.js

--- app/src/inject/inject.js
+++ app/src/inject/inject.js
@@ -1,8 +1,4 @@
-chrome.extension.sendMessage({}, function(response) {
-	
-	var s = document.createElement('script');
-	s.src = chrome.extension.getURL('src/inject/script.js');
-	s.async = true;
-	document.body.appendChild(s);
-
-});
+var s = document.createElement('script');
+s.src = chrome.extension.getURL('src/inject/script.js');
+s.async = true;
+document.body.appendChild(s);
